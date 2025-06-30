a238f3f161e783b3e52fb7e3c4b0a4d0287a0153
RodrigoKNZ <a20200554@pucp.edu.pe>
2025-06-29 20:28:45 -0500

    Vercel cambio 6

--- index.js
+++
@@ -1,7 +0,0 @@
-// Punto de entrada para Vercel
-// Este archivo redirige al backend
-const path = require('path');
-const backendPath = path.join(__dirname, 'Server', 'backend', 'index.js');
-
-// Importar y exportar el backend
-module.exports = require(backendPath); 
